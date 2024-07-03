1b74e572abcf89696f11b22e19d852c78bad062c
ca-mi-lo <camilo.ab.sc@gmail.com>
2024-07-03 17:35:01 -0600

    Practice Exercise 2 ... Van 6 de 10

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # OOP_Python
+
+This notes are taken from this course
+[https://www.udemy.com/course/object-oriented-python-programming/learn/lecture/18241930#learning-tools]
---
+++ _04_person.py
@@ -0,0 +1,22 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def display(self):
+        print(self.name, self.age)
+    
+    @property
+    def age(self):
+        return self._age
+    
+    @age.setter
+    def age(self, new_age):
+        if 20 < new_age <80:
+            self._age = new_age
+        else:
+            raise ValueError('Age must be between 20 and 80')
+
+if __name__ == '__main__':
+    p = Person('BobSponge', 25)
+    p.display()
